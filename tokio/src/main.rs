ef19ff1fd7e34a04c6ee90f9dbfe14689c8d6c18
novin <cocolupiayi@gmail.com>
2023-07-31 00:49:52 +0800

    update tokio setup

--- tokio/src/main.rs
+++ tokio/src/main.rs
@@ -1,8 +1,21 @@
+use std::io::Error;
+
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::runtime::Runtime;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create the runtime
     let rt = Runtime::new()?;
-    loop {}
-    // Ok(())
+
+    // Spawn the main task thread
+    let result = rt.block_on(async {
+        let _listener = TcpListener::bind("127.0.0.1:8080").await?;
+        let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+        stream.write_all(b"hello world!").await?;
+        println!("test1");
+        Ok::<(), Error>(())
+    });
+    Ok(result.unwrap())
 }
