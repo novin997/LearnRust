d4a3245b1b5d2f20a589bb1b31866fa78c378af3
novin <cocolupiayi@gmail.com>
2023-08-06 14:26:56 +0800

    Test Tcp Connection with nc

--- tokio/src/main.rs
+++ tokio/src/main.rs
@@ -1,21 +1,34 @@
-use std::io::Error;
+use std::thread;
 
 use tokio::io::AsyncWriteExt;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpStream;
 use tokio::runtime::Runtime;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create the runtime
     let rt = Runtime::new()?;
 
     // Spawn the main task thread
-    let result = rt.block_on(async {
-        let _listener = TcpListener::bind("127.0.0.1:8080").await?;
-        let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
-        stream.write_all(b"hello world!").await?;
-        println!("test1");
-        Ok::<(), Error>(())
+    rt.block_on(async {
+        // let _listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+        let mut stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+        loop {
+            let sum = expensive_operation();
+            println!("{sum}");
+            stream.write_all(&sum.to_string().as_bytes()).await.unwrap();
+            thread::sleep(Duration::from_millis(2000));
+        }
+        // Ok::<(), Error>(())
     });
-    Ok(result.unwrap())
+    Ok(())
+}
+
+fn expensive_operation() -> i32 {
+    let mut val = 0;
+    for i in 1..10000 {
+        val = val + i;
+    }
+    val
 }
